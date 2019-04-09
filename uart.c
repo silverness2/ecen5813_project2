3415666e9202adc02a5bdd7a189f789c8e94eb96
Shilpi Gupta <shilpi.gupta@colorado.edu>
2019-04-09 02:34:31 -0600

    Uncommented receive config in uart_init()

--- uart.c
+++ uart.c
@@ -6,7 +6,7 @@ int uart_init()
     // Enable clock for UART0.
     // Clock register for UARTs found in the SIM_SCGC4 (System Clock Gating
     // Control) register. UART0 clock is at bit 10.
-    SIM->SCGC4 |= SIM_SCGC4_UART0_MASK; // mask is 0x400 = bit 10
+    SIM->SCGC4 |= SIM_SCGC4_UART0_MASK; // mask = 0x400 = bit 10
 
     // Set source for baud rate generator clock for UART0 as FLL (41.94 MHz).
     // FLL = Frequency Locked Loop (vs. PLL = Phase Locked Loop)
@@ -26,41 +26,39 @@ int uart_init()
     // * Should only be changed when transmitter and receiver are both disabled.
     UART0->C4 = 0x0F;
 
-    // Set control register value: no parity (bit 1), 8-bit data size and 1 stop
-    // bit (bit 4).
+    // Set control register flags:
+    // no parity (bit 1), 8-bit data size and 1 stop bit (bit 4)
     UART0->C1 = 0x00;
 
-    // Enable the transmitter for UART0.
-    UART0->C2 = 0x08; // bit 4 is for TE (transmit enable)
+    // Enable the transmitter for UART0, TE (Transmit Enable)
+    UART0->C2 |= UART0_C2_TE_MASK; // mask = 0x8 = bit 4 is for TE
 
-// Enable the receiver for UART0.
-//UART->C2 = 0x04; // bit 3 is for RE (receive enable)
+    // Enable the receiver for UART0, RE (Receive Enable).
+    UART0->C2 |= UART0_C2_RE_MASK; // mask = 0x4 = bit 3 is for RE
 
     // Enable clock for PORTA.
-    SIM->SCGC5 |= SIM_SCGC5_PORTA_MASK; // mask is 0x200 = bit 9
+    SIM->SCGC5 |= SIM_SCGC5_PORTA_MASK; // mask = 0x200 = bit 9
 
     // Select alt func 2 (ALT2) (bits 10-8: MUX = 010) for PA2 (UART0_Tx) pins.
     PORTA->PCR[2] |= 0x0200;
 
-// Select alt func 2 (ALT2) (bits 10-8: MUX = 010) for PA1 (UART0_Tx) pins.
-//PORTA->PCR[1] |= 0x0200;
+    // Select alt func 2 (ALT2) (bits 10-8: MUX = 010) for PA1 (UART0_Tx) pins.
+    PORTA->PCR[1] |= 0x0200;
 }
 
 int uart_can_transmit()
 {
-    // Method 1
-    // The TC (Transmit Complete) flag (bit 6):
-    // 0 = Transmission in porgress (shift reg occupied)
-    // 1 = No transmission in progress (both shift reg and data reg empty)
-    //if ((UART0->S1 & 0x40) == 0)
-    // * Shouldn't I use Method 1?
-
-    // Method 2
-    // The TDRE (Transmit Data Register Empty) flag (bit 7):
+    // The TDRE (Transmit Data Register Empty) flag (bit 7 = 0x80):
     // 0 = Shift register is loaded and shifting. An additional byte is waiting
     //     in the Data Register.
     // 1 = The Data Register is empty and ready for the next byte.
-    if ((UART0->S1 & 0x80) == 0)
+
+    // The TC (Transmit Complete) flag (bit 6 = 0x40):
+    // 0 = Transmission in porgress (shift reg occupied)
+    // 1 = No transmission in progress (both shift reg and data reg empty)
+
+    if ((UART0->S1 & UART0_S1_TDRE_MASK == 0) || // 0x80
+        (UART0->S1 & UART0_S1_TC_MASK   == 0)) // 0x40
     {
         return 0; // shift reg still loaded
     }
@@ -74,8 +72,7 @@ void uart_transmit(char c)
 {
     if (uart_can_transmit)
     {
-        // Transmit character (writing to this reg initiates a transmission from
-        // the uart).
+        // Transmit char. Writing to this reg starts a transmission from uart.
         UART0_D = c;
     }
 }
